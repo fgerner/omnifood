678cc2d7682af497d7638b5eb3ac8771501d7086
Fred <fred.gerner@icloud.com>
2020-09-10 22:26:46 +1000

    added links to sections

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,9 +1,18 @@
 $(document).ready(function () {
     $('.js-section-features').waypoint(function (direction) {
-        if (direction == "down") {
+        if (direction === "down") {
             $('nav').addClass('sticky');
         } else {
             $('nav').removeClass('sticky');
         }
+    }, {
+        offset: '60px'
     });
+
+    $('.js-scroll-to-plans').click(function () {
+        $('html, body').animate({scrollTop: $('.section-plans').offset().top})
+    })
+    $('.js-scroll-to-start').click(function () {
+        $('html, body').animate({scrollTop: $('.section-features').offset().top})
+    })
 });
