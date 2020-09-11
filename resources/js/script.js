e90c855fa6956703beca5bcb627505f5fbbbf20a
Fred <fred.gerner@icloud.com>
2020-09-11 20:02:01 +1000

    added  animation to sections

--- resources/js/script.js
+++ resources/js/script.js
@@ -15,4 +15,25 @@ $(document).ready(function () {
     $('.js-scroll-to-start').click(function () {
         $('html, body').animate({scrollTop: $('.section-features').offset().top})
     })
+
+    $('.js-wp-1').waypoint(function (direction) {
+        $('.js-wp-1').addClass('animated animate__pulse');
+    }, {
+        offset: '50%'
+    })
+    $('.js-wp-2').waypoint(function (direction) {
+        $('.js-wp-2').addClass('animated animate__fadeInUp');
+    }, {
+        offset: '50%'
+    })
+    $('.js-wp-3').waypoint(function (direction) {
+        $('.js-wp-3').addClass('animated animate__fadeIn');
+    }, {
+        offset: '50%'
+    })
+    $('.js-wp-4').waypoint(function (direction) {
+        $('.js-wp-4').addClass('animated animate__pulse');
+    }, {
+        offset: '50%'
+    })
 });
